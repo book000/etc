32fb5b1754d0d6c76c37465e038cdfd6b9a48a4d
tomachi <book000@users.noreply.github.com>
2016-03-25 01:06:27 +0900

    Create minecraft_some_command.php

---
+++ minecraft_some_command.php
@@ -0,0 +1,69 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+$list = "/say 0$/say 1$/say 2$/say 3$/say 4$/say 5$/say 6$/say 7$/say 8";
+$commands = explode("$", $list);
+$arr = array();
+$c = 1;
+$fill = 0;
+krsort($commands);
+reset($commands);
+foreach($commands as $command){
+    re:
+    if($c < 2){
+        // command_block(cb)
+        if($command == ""){continue;}
+        $arr[] = "cb:".$command;
+        $c++;
+        $fill++;
+    }else{
+        // redstone_block(rb)
+        $arr[] = "rb:";
+        $c = 0;
+        $fill++;
+        goto re;
+ 
+   }
+}
+$fill++;
+$fill++;
+if($c == 1){
+    $fill++;
+}else if($c == 2){
+    $fill++;
+}
+if($c == 0){
+    $text = "/summon FallingSand ~2 ~0 ~0 {Time:127b,Block:minecraft:command_block,Data:0b,TileEntityData:{TrackOutput:1b,Command:\"\"},";
+    $textend = "}";
+}else if($c == 1){
+    $text = "/summon FallingSand ~2 ~0 ~0 {Time:127b,Block:minecraft:redstone_block,Data:0b,TileEntityData:{TrackOutput:1b},";
+    $textend = "}";
+}else if($c == 2){
+    $text = "/summon FallingSand ~2 ~0 ~0 {Time:127b,Block:minecraft:redstone_block,Data:0b,TileEntityData:{TrackOutput:1b},";
+    $textend = "}";
+}
+    //$text .= "Riding:{id:FallingSand,Time:127b,Block:minecraft:redstone_block,Data:0b,TileEntityData:{TrackOutput:1b},";
+//$textend .= "}";
+    $text .= "Riding:{id:FallingSand,Time:127b,Block:minecraft:command_block,Data:0b,TileEntityData:{TrackOutput:1b,Command:\"/fill ~ ~2 ~ ~ ~-".$fill." ~ air\"},";
+    $textend .= "}";
+
+foreach ($arr as $data) {
+    $data = explode(":", $data);
+    if($data[0] == "cb"){
+        $text .= "Riding:{id:FallingSand,Time:127b,Block:minecraft:command_block,Data:0b,TileEntityData:{TrackOutput:1b,Command:\"".$data[1]."\"},";
+    }else if($data[0] == "rb"){
+        $text .= "Riding:{id:FallingSand,Time:127b,Block:minecraft:redstone_block,Data:0b,TileEntityData:{},";
+    }
+    $textend .= "}";
+}
+$text .= "Riding:{id:FallingSand,Time:127b,Block:minecraft:redstone_block,Data:0b,TileEntityData:{TrackOutput:1b},";
+$textend .= "}";
+$text .= "Riding:{id:FallingSand,Time:127b,Block:minecraft:command_block,Data:0b,TileEntityData:{TrackOutput:1b,Command:\"\"},";
+$textend .= "}";
+if($data[0] == "cb"){
+    $text .= "Riding:{id:FallingSand,Time:127b,Block:minecraft:redstone_block,Data:0b,TileEntityData:{TrackOutput:1b},";
+    $textend .= "}";
+}
+
+$text = substr($text, 0, -1);
+$all = $text.$textend;
+echo $all;
