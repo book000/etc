d3f73011472bee6ef83315f2aa8afa0f9cc3adae
tomachi <book000@users.noreply.github.com>
2018-07-14 21:57:49 +0900

    Create SteamGame.class.php

---
+++ SteamGame.class.php
@@ -0,0 +1,51 @@
+<?php
+
+class SteamGame {
+    private $appid = null;
+    private $gameData = null;
+    function __construct($appid){
+        $this->appid = $appid;
+        $context = stream_context_create(
+            array(
+                "http"=> array(
+                    "method" => "GET",
+                    "ignore_errors" => true
+                )
+            )
+        );
+        $json = file_get_contents("https://store.steampowered.com/api/appdetails?appids=" . $appid, false, $context);
+        preg_match('/HTTP\/1\.[0|1|x] ([0-9]{3})/', $http_response_header[0], $matches);
+        $status_code = $matches[1];
+        if($status_code != 200){
+            throw new Exception("Failed to get SteamGameData. (" . $http_response_header[0] . ")");
+        }
+        $array = json_decode($json, true);
+        $this->gameData = $array[$appid];
+        if(!$this->gameData["success"]){
+            throw new Exception("Failed to get SteamGameData.");
+            $this->gameData = null;
+        }
+    }
+    function getFinalPrice(){
+        if($this->gameData == null){
+            throw new Exception("Failed to get SteamGameData.");
+        }
+        return $this->gameData["data"]["price_overview"]["final"];
+    }
+    function getCurrencyPrice(){
+        if($this->gameData == null){
+            throw new Exception("Failed to get SteamGameData.");
+        }
+        return $this->gameData["data"]["price_overview"]["final"];
+    }
+    
+    function getDiscountPercent(){
+        if($this->gameData == null){
+            throw new Exception("Failed to get SteamGameData.");
+        }
+        return $this->gameData["data"]["price_overview"]["discount_percent"];
+    }
+}
+
+$gameData = new SteamGame("578080");
+echo $gameData->getDiscountPercent() . "%";
