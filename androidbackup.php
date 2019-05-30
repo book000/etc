8788dd5e47100c1ade04e1f48b8e26001d24a007
Tomachi <book000@users.noreply.github.com>
2019-05-31 06:39:56 +0900

    aaptによるデータ取得対応。データ取得元制御追加(プレイストア)。デバッグ出力追加

--- androidbackup.php
+++ androidbackup.php
@@ -8,6 +8,8 @@
   複数端末接続非対応
  */
 
+$playstore_use_flag = true; // アプリ名の取得にプレイストアを利用するか？
+
 function pp($text){
     $text = trim($text);
     echo date("Y/m/d H:i:s") . " | " . $text . "\n";
@@ -35,6 +37,9 @@ if (!file_exists(__DIR__ . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR)) 
 if (!file_exists(__DIR__ . DIRECTORY_SEPARATOR . "sdcarddata" . DIRECTORY_SEPARATOR)) {
     mkdir(__DIR__ . DIRECTORY_SEPARATOR . "sdcarddata" . DIRECTORY_SEPARATOR);
 }
+if (!file_exists(__DIR__ . DIRECTORY_SEPARATOR . "tmp" . DIRECTORY_SEPARATOR)) {
+    mkdir(__DIR__ . DIRECTORY_SEPARATOR . "tmp" . DIRECTORY_SEPARATOR);
+}
 
 // モデル名取得
 $model = shell_exec("adb shell getprop ro.product.model");
@@ -56,6 +61,14 @@ $x = $win_x - 300;
 $y = $win_y - 100;
 
 pp("Window Size: " . $win_x . " x " . $win_y);
+
+exec("aapt > nul 2>&1", $_out, $return_var);
+$aapt_enable = ($return_var != 1);
+if($aapt_enable){
+    pp("aapt: Available");
+}else{
+    pp("aapt: Unavailable");
+}
 
 // プレイストアアクセス時のエラー抑制
 $context = stream_context_create(array(
@@ -81,13 +94,21 @@ foreach ($adblists as $key => $adb) {
     $apkFile = mb_substr($adb, 0, $pos); // =以前がapkファイルのパス
     $apkFile = trim($apkFile);
 
+    pp("[" . ($key + 1) . "/" . count($adblists) . "] ---------- " . $id . " -----");
+
     // プレイストアにアクセス
-    $html = file_get_contents("https://play.google.com/store/apps/details?hl=ja_JP&id=" . $id, false, $context);
+    if($playstore_use_flag){
+        $html = file_get_contents("https://play.google.com/store/apps/details?hl=ja_JP&id=" . $id, false, $context);
 
-    preg_match('/HTTP\/1\.[0|1|x] ([0-9]{3})/', $http_response_header[0], $matches);
-    $status_code = $matches[1]; // ステータスコード取得
-
+        preg_match('/HTTP\/1\.[0|1|x] ([0-9]{3})/', $http_response_header[0], $matches);
+        $status_code = $matches[1]; // ステータスコード取得
+    }else{
+        $status_code = -1;
+    }
+    
+    unset($name);
     if ($status_code == 200) { // ステータスコードが200だったら
+        pp("[" . ($key + 1) . "/" . count($adblists) . "] GooglePlay StatusCode: 200");
         preg_match("/<h1.* itemprop=\"name\"><span >(.+)<\/span><\/h1>/", $html, $m);
         $name = html_entity_decode($m[1]); // アプリ名取得
         $nameORId = replaceFilenameillegalChar(
@@ -99,17 +120,57 @@ foreach ($adblists as $key => $adb) {
             rename(__DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . replaceFilenameillegalChar($m[1]), __DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $nameORId);
             pp("[" . ($key + 1) . "/" . count($adblists) . "] " . "RENAME FROM " . "apk" . DIRECTORY_SEPARATOR . replaceFilenameillegalChar($m[1]) . " TO " . "apk" . DIRECTORY_SEPARATOR . $nameORId . "!");
         }
-    } else {
-        $name = null; // プレイストアから取得できなかったらnull
+    }
+    if(!isset($name) && $aapt_enable) {
+        // apkを引っ張ってきてaaptで調べる
+        pp("[" . ($key + 1) . "/" . count($adblists) . "] Try to get data from aapt...");
+        system("adb shell cp \"" . $apkFile . "\" /storage/emulated/0/APK");
+        $filename = substr($apkFile, strrpos($apkFile, "/") + 1);
+        $output = exec("adb pull -p \"/storage/emulated/0/APK/" . $filename . "\" -p \"" . __DIR__ . DIRECTORY_SEPARATOR . "tmp" . DIRECTORY_SEPARATOR . "tmp.apk\"");
+        pp($output);
+        if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . "tmp" . DIRECTORY_SEPARATOR . "tmp.apk")){
+            // ok
+            $packageaaptData = shell_exec("aapt dump badging " . __DIR__ . DIRECTORY_SEPARATOR . "tmp" . DIRECTORY_SEPARATOR . "tmp.apk");
+                unset($m);
+            preg_match("/application-label-ja:'(.+)'/", $packageaaptData, $m);
+            if(isset($m[1])){
+                $name = html_entity_decode($m[1]); // アプリ名取得
+                $nameORId = replaceFilenameillegalChar(
+                    html_entity_decode(
+                        $m[1]
+                    )
+                );
+                pp("[" . ($key + 1) . "/" . count($adblists) . "] Successful data acquisition from aapt! (ja)");
+            }else{
+                unset($m);
+                preg_match("/application-label:'(.+)'/", $packageaaptData, $m);
+                if(isset($m[1])){
+                    $name = html_entity_decode($m[1]); // アプリ名取得
+                    $nameORId = replaceFilenameillegalChar(
+                        html_entity_decode(
+                            $m[1]
+                        )
+                    );
+                    pp("[" . ($key + 1) . "/" . count($adblists) . "] Successful data acquisition from aapt! (other lang)");
+                }
+            }
+            system("adb shell rm \"/storage/emulated/0/APK/" . $filename . "\"");
+            unlink(__DIR__ . DIRECTORY_SEPARATOR . "tmp" . DIRECTORY_SEPARATOR . "tmp.apk");
+        }
+    }
+    if(!isset($name) || $name == null){
+        pp("[" . ($key + 1) . "/" . count($adblists) . "] Failed to get application name. Use application ID.");
+        $name = "null"; // プレイストアもaaptからも取得できなかったらnull
         $nameORId = $id;
     }
-    
+
     // 端末からアプリのバージョンを取得する
     $packageData = shell_exec("adb shell dumpsys package " . $id);
     preg_match("/versionName=(.+)/", $packageData, $m);
     $versionName = $m[1];
 
-    pp("[" . ($key + 1) . "/" . count($adblists) . "] " . $name . " " . $id . " " . $versionName);
+    pp("[" . ($key + 1) . "/" . count($adblists) . "] Application Name: " . $name);
+    pp("[" . ($key + 1) . "/" . count($adblists) . "] Version: " . $versionName);
     //echo __DIR__ . "/apk/" . $id . "_" . $versionName . ".apk";
 
     $backupList[] = [
@@ -127,19 +188,29 @@ foreach ($adblists as $key => $adb) {
     $versionName = replaceFilenameillegalChar($versionName);
 
     if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $nameORId . DIRECTORY_SEPARATOR . $versionName . ".apk")) {
-        pp("APK File is found!");
+        pp("[" . ($key + 1) . "/" . count($adblists) . "] APK File is found!");
         continue;
     }
 
     // APKファイルダウンロード
+    pp("[" . ($key + 1) . "/" . count($adblists) . "] APK File Downloading...");
+
     system("adb shell cp \"" . $apkFile . "\" /storage/emulated/0/APK");
     $filename = substr($apkFile, strrpos($apkFile, "/") + 1);
     $output = exec("adb pull -p \"/storage/emulated/0/APK/" . $filename . "\" -p \"" . __DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $nameORId . DIRECTORY_SEPARATOR . $versionName . ".apk\"");
     pp($output);
-    if(!file_exists(__DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $nameORId . DIRECTORY_SEPARATOR . $versionName . ".apk")){
+    if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $nameORId . DIRECTORY_SEPARATOR . $versionName . ".apk")){
+        pp("[" . ($key + 1) . "/" . count($adblists) . "] APK File Download Successful! (ApplicationName)");
+    }else{
         // download error
+        if (!file_exists(__DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $id)) {
+            mkdir(__DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $id);
+        }
         $output = exec("adb pull -p \"/storage/emulated/0/APK/" . $filename . "\" -p \"" . __DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $id . DIRECTORY_SEPARATOR . $versionName . ".apk\"");
         pp($output);
+        if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . "apk" . DIRECTORY_SEPARATOR . $id . DIRECTORY_SEPARATOR . $versionName . ".apk")){
+            pp("[" . ($key + 1) . "/" . count($adblists) . "] APK File Download Successful! (ApplicationID)");
+        }
     }
     system("adb shell rm \"/storage/emulated/0/APK/" . $filename . "\"");
 
